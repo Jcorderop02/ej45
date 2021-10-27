180526e9e2327b034dec6ad6d1390440dfebfddc
Juan <jcorderop@hotmail.es>
2021-10-27 11:01:55 +0200

    Terminado que salga la excepcion al poner una letra y que vuelva a pedir el numero

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -17,14 +17,44 @@ limitations under the License.*/
  */
 package aplicacion;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main (String[] args){
-        System.out.println("Introduzca su nombre:");
-        Scanner sc = new Scanner(System.in);
-        String nombre = sc.next();
-        System.out.println("Su nombre es: " + nombre);
-        sc.close();
+    public static void main(String[] args) {
+        /*try {
+            System.out.println("Introduzca dos numeros:");
+            Scanner sc = new Scanner(System.in);
+            Integer numero = sc.nextInt();
+            Integer numero2 = sc.nextInt();
+            int suma = numero + numero2;
+            System.out.println("El total de la suma es: " + suma);
+            sc.close();
+        }catch (InputMismatchException exception){
+            System.out.println("Ha introducido una letra");
+        }*/
+
+        //Intentando hacer lo de arriba con un bucle
+
+        boolean verdadero = true;
+        while(verdadero){
+            //for(boolean verdadero = true ;verdadero;) {
+                //int x = 0;
+            try {
+                Scanner sc2 = new Scanner(System.in);
+                int suma = 0;
+
+                for (int i = 0; i < 2; i++) {
+                    System.out.println("Introduzca un numero entero");
+                    suma += sc2.nextInt();
+                }
+                System.out.println("La suma total es: " + suma);
+                verdadero = false;
+                //x++;
+            } catch (InputMismatchException exception) {
+                System.out.println("Ha introducido una letra");
+                verdadero = true;
+            }
+        }
     }
 }
